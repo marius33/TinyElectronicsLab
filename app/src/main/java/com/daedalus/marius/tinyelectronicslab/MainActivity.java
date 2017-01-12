8187c07b5c2987fe247310710ee8801baefd5de2
Marius Lazar <marius.lazar33@gmail.com>
2017-01-12 18:30:13 +0200

    Recommit

---
+++ app/src/main/java/com/daedalus/marius/tinyelectronicslab/MainActivity.java
@@ -0,0 +1,13 @@
+package com.daedalus.marius.tinyelectronicslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
